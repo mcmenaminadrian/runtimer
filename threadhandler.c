4f1c986356991dc139844ef6467b097dfe87b95b
Adrian McMenamin <adrianmcmenamin@gmail.com>
2014-02-23 23:59:42 +0000

    more thread code

---
+++ threadhandler.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <expat.h>
+#include <pthread.h>
+
+
+//C code that parses a thread
+
+void* startThreadHandler(void *resources)
+{
+
+	return NULL;
+}
