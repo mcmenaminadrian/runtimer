df19c33b64e20fe8ec27c0ece3519a30b08afd5b
Adrian McMenamin <adrianmcmenamin@gmail.com>
2014-02-26 23:36:50 +0000

    building thread parser

--- threadhandler.c
+++ threadhandler.c
@@ -10,6 +10,17 @@ void* startThreadHandler(void *resources)
 {
 	struct threadResources thResources;
 	thResources = (struct threadResources*)resources;
+	printf("Setting up parser for thread %i\n",
+		thResources->local->threadNumber);
+	//Setup the Parser
+	XML_Parser p_threadParser = XML_ParserCreate("UTF-8");
+	if (!p_threadParser) {
+		fprintf(stderr, "Could not create parser for thread\n");
+		return (void*)(-1);
+	}
+	//Pass the parser thread specific data
+	XML_SetUserData(p_threadParser, resources);
+	//Start the Parser
 
 	return NULL;
 }
