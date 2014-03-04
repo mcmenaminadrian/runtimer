ac52326b9b9f35e1da4dc38e6bbbef0b8e44b993
Adrian McMenamin <adrianmcmenamin@gmail.com>
2014-03-04 17:22:31 +0000

    another small fix

--- pages.h
+++ pages.h
@@ -9,13 +9,13 @@ void* createPageTree(void);
 void removePageTree(void* tree);
 inline void* getRootPageTree(void* tree);
 inline void* getRootPageTreeLRU(void* tree);
-void insertIntoPageTree(long pageNumber, long lruTime, void* tree);
+void insertIntoPageTree(long pageNumber, time_t lruTime, void* tree);
 void* locatePageTreePR(long pageNumber, void* tree);
 void* locatePageTreeLRU(long pageNumber, void* tree);
 void removeFromPageTree(long pageNumber, void* tree);
 void* removeOldestFromPageTree(void* tree);
 int countPageTree(void* tree);
-void updateLRU(long pageNumber, long lruTime, void* tree);
+void updateLRU(long pageNumber, time_t lruTime, void* tree);
 struct PageChain* getPageChain(void* tree);
 
 #endif
