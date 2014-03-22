5bc3303627eb0436dd21ba6ff02bf182e3d378b8
Adrian McMenamin <adrianmcmenamin@gmail.com>
2014-03-22 20:52:28 +0000

    first cut of new OPT code

--- insttree.cpp
+++ insttree.cpp
@@ -18,36 +18,8 @@ void insertIntoTree(long pageNumber, long instruction, void* tree)
 {
 	map<long, long>* instTree;
 	instTree = static_cast<map<long, long>*>(tree);
-	map<long, long>::iterator it;
-	it = instTree->find(pageNumber);
-	if (it != instTree->end()) {
-		instTree->at(pageNumber) = instruction;
-	} else {
-		instTree->insert(pair<long, long>(pageNumber, instruction));
+	instTree->insert(pair<long, long>(pageNumber, instruction));
 	}
-}
-
-long maxNodePage(void* tree)
-{
-	map<long, long>* instTree;
-	instTree = static_cast<map<long, long>*>(tree);
-	map<long, long>::reverse_iterator rit = instTree->rbegin();
-	return rit->first;
-}
-
-long maxNodeDistance(void* tree)
-{
-	map<long, long>* instTree;
-	instTree = static_cast<map<long, long>*>(tree);
-	map<long, long>::reverse_iterator rit = instTree->rbegin();
-	return rit->second;
-}
-
-long closestPage(void* tree)
-{
-	map<long, long>* instTree;
-	instTree = static_cast<map<long, long>*>(tree);
-	return instTree->begin()->first;
 }
 
 void freeInstTree(void* tree)
@@ -56,7 +28,58 @@ void freeInstTree(void* tree)
 	instTree = static_cast<map<long, long>*> (tree);
 	delete instTree;
 }
-		
+
+void* createMinTree(void)
+{
+	map<long, long>* minTree = new map<long, long>();
+	return static_cast<void *>(minTree);
+}
+
+void killMinTree(void* tree)
+{
+	map<long, long>* minTree = static_cast<map<long, long>*>(tree);
+	delete minTree;
+}
+
+void pushToMinTree(void* mTree, void* iTree)
+{
+	map<long, long>* minTree;
+	map<long, long>* instTree;
+	minTree = static_cast<map<long, long>*>(mTree);
+	iTree = static_cast<map<long, long>*>(iTree);
+	map<long, long>::iterator itInst;
+	//insert into tree if no record for page or a new min distance
+	for (itInst = iTree->begin(); itInst != iTree->end(); itInst++)
+	{
+		map<long, long>::iterator itMin = minTree->find(itInst->first);
+		if (itMin == minTree->end()) {
+			minTree->insert(
+				pair<long, long>(itInst->first,
+				itInst->second));
+		} else {
+			if (itMin->second > itInst->second) {
+				itMin->second = itInst->second;
+			}
+		}
+	}
+}
+
+long getPageToKill(void* tree)
+{
+	//find the page with the greatest distance
+	long maxDistance = 0;
+	long maxPage = 0;
+	map<long, long>* minTree = static_cast<map(long, long)*>(tree);
+	map<long, long>::iterator it;
+	for (it = minTree->begin; it != minTree->end(); it++) {
+		if (it->second > maxDistance) {
+			maxDistance = it->second;
+			maxPage = it->first;
+		}
+	}
+	return maxPage;
+}
+
 };  //end extern "C"		
 
 
